594e56eaaea779899757b309c67a3bbcd2f7f6bd
Marcin Olichwiruk <marcin@opensoftware.pl>
2021-10-19 12:41:28 +0200

    Add navigation module

---
+++ src/modules/navigation/Nav.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { PlaylistRepo } from './repositories/PlaylistRepo'
+
+  const playlistRepo = new PlaylistRepo()
+  let playlistsPromise = playlistRepo.getPlaylists()
+</script>
+
+<main>
+  {#await playlistsPromise then playlists}
+    {#each playlists as playlist}
+      <a href="/playlist/{playlist.id}">{playlist.name}</a>
+      <br>
+    {/each}
+  {/await}
+</main>
+
+<style lang="scss">
+  main {
+    text-align: left;
+    padding: 1em;
+    max-width: 340px;
+    margin: 0 0;
+  }
+</style>
